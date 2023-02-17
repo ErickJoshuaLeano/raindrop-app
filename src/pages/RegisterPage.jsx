bd81ea3a967c7611fd8d5d29091150dc44ee67f5
Marie Cristine Torres <116280260+mrcrstndtrrs@users.noreply.github.com>
2023-02-17 23:35:43 +0800

    First Commit

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,152 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Grid,
+  TextField,
+} from "@mui/material";
+import Joi from "joi";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as authService from "../services/auth";
+
+const RegisterPage = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const [errors, setErrors] = useState({});
+
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await authService.register(
+        form.name,
+        form.email,
+        form.username,
+        form.password
+      );
+      alert("Registration successful");
+      navigate("/login");
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        alert(error.response.data.message);
+      }
+    }
+  };
+
+  const handleChange = ({ currentTarget: input }) => {
+    setForm({
+      ...form,
+      [input.name]: input.value,
+    });
+
+    const { error } = schema
+      .extract(input.name)
+      .label(input.name)
+      .validate(input.value);
+
+    if (error) {
+      setErrors({ ...errors, [input.name]: error.details[0].message });
+    } else {
+      delete errors[input.name];
+      setErrors(errors);
+    }
+  };
+
+  const isFormInvalid = () => {
+    const result = schema.validate(form);
+
+    return !!result.error;
+  };
+
+  return (
+    <Grid
+      container
+      component="form"
+      justifyContent="center"
+      onSubmit={handleSubmit}
+    >
+      <Grid item xs={6}>
+        <Card>
+          <CardHeader title="Register" />
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  name="name"
+                  error={!!errors.name}
+                  helperText={errors.name}
+                  onChange={handleChange}
+                  value={form.name}
+                  label="Name"
+                  variant="standard"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  error={!!errors.email}
+                  helperText={errors.email}
+                  onChange={handleChange}
+                  value={form.email}
+                  label="Email"
+                  variant="standard"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="username"
+                  error={!!errors.username}
+                  helperText={errors.username}
+                  onChange={handleChange}
+                  value={form.username}
+                  label="Username"
+                  variant="standard"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  error={!!errors.password}
+                  helperText={errors.password}
+                  onChange={handleChange}
+                  value={form.password}
+                  label="Password"
+                  variant="standard"
+                  type="password"
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+          <CardActions>
+            <Button disabled={isFormInvalid()} type="submit" fullWidth>
+              Sign up
+            </Button>
+            <Button>Need Help?</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default RegisterPage;
