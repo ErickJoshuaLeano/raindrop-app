e27d83bbf506c0a498fa7ace8eb0ac9cd2c9bff5
krikris77 <125444899+krikris77@users.noreply.github.com>
2023-02-27 17:04:35 +0800

    Added Not Found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import HomeProfileCard from "../components/Home Page/HomeProfileCard";
+import NavBar from "../components/NavBar";
+import * as authService from "../services/auth";
+import "./HomePages.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useTheme } from "@mui/material/styles";
+import * as profilesService from "../services/profile";
 
 const NotFound = () => {
-  return <h1>Page does not exist</h1>;
+  const theme = useTheme();
+  const currentUser = authService.getCurrentUser();
+  const [thisUser, setThisUser] = useState([]);
+  const [accessToken, setAccessToken] = useState(authService.getAccessToken());
+  const [isLoading, setLoading] = useState(true);
+  const [isLoadingUser, setLoadingUser] = useState(true);
+  const [updatePage, setUpdatePage] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    profilesService.fetchCurrentUser().then((response) => {
+      setThisUser(response.data);
+      setLoadingUser(false);
+      setUpdatePage(false);
+    });
+  }, [updatePage]);
+
+  const handleLogout = () => {
+    authService.logout();
+    setAccessToken(null);
+    navigate("/login");
+    window.location.reload(false);
+    setLoading(false);
+    setLoadingUser(false);
+  };
+
+  return (
+    <>
+      <div
+        className="background"
+        style={{ backgroundColor: theme.palette.background.main }}
+      >
+        <NavBar
+          onLogout={handleLogout}
+          thisUser={thisUser}
+          updatePage={updatePage}
+          setUpdatePage={setUpdatePage}
+        />
+      </div>
+      <div class="drop-container">
+        <h1>PAGE NOT FOUND</h1>
+        <div class="drop"></div>
+      </div>
+    </>
+  )
 };
 
 export default NotFound;
